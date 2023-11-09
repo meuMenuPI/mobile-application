857699d067afd442477369e2cad8f5a9fc69b011
lsoaress <lucassoaresdecarvalho2013@gmail.com>
2023-11-08 21:09:10 -0300

    barra de busca finalizada

--- app/src/main/java/com/example/mobile_application/MenuPrincipal.kt
+++ app/src/main/java/com/example/mobile_application/MenuPrincipal.kt
@@ -6,10 +6,13 @@ import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SearchView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.view.setPadding
@@ -17,6 +20,7 @@ import com.example.mobile_application.adapter.MiniEspecialidadeAdapter
 import com.example.mobile_application.adapter.MiniRestauranteAdapter
 import com.example.mobile_application.api.Rest
 import com.example.mobile_application.databinding.ActivityMenuPrincipalBinding
+import com.example.mobile_application.models.RestauranteDto
 import com.example.mobile_application.models.RestauranteReviewDto
 import com.example.mobile_application.service.RestauranteService
 import retrofit2.Call
@@ -33,9 +37,12 @@ class MenuPrincipal : AppCompatActivity() {
     private val retrofit by lazy {
         Rest.getInstance().create(RestauranteService::class.java)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         binding.btnPerfil.setOnClickListener {
             val i = Intent(
@@ -62,13 +69,70 @@ class MenuPrincipal : AppCompatActivity() {
             startActivity(i)
         }
 
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        var lista: MutableList<String> = mutableListOf()
+        val listaRes = binding.listaRestaurante
+        var listAdapter: ArrayAdapter<String>
 
-        //val constraintLayoutCultura = ConstraintLayout(baseContext)
-        //val constraintLayoutPerto = ConstraintLayout(baseContext)
+
+        retrofit.pegarRestaurante().enqueue(object : Callback<List<RestauranteDto>> {
+            override fun onResponse(
+                call: Call<List<RestauranteDto>>,
+                response: Response<List<RestauranteDto>>
+            ) {
+
+                Log.d("RESPOSTA", response.body().toString())
+
+                var listaTemp: List<RestauranteDto>? = response.body()
+                listaTemp?.forEach { item ->
+                    lista.add(item.nome)
+                }
+
+                Log.d("LISTATEMP", listaTemp.toString())
+            }
+
+            override fun onFailure(call: Call<List<RestauranteDto>>, t: Throwable) {
+                Log.d("T_RESPONSE_ERRO", t.toString())
+            }
+
+        })
+
+        listAdapter = ArrayAdapter<String>(
+            this@MenuPrincipal,
+            android.R.layout.simple_list_item_1,
+            lista
+        )
+        listaRes.adapter = listAdapter
+
+        binding.barraPesquisa.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                if (lista?.contains(query)!!) {
+                    listAdapter.filter.filter(query)
+                    listaRes.visibility = View.VISIBLE
+                } else {
+                    listaRes.visibility = View.INVISIBLE
+                    Toast.makeText(this@MenuPrincipal, "Restaurante não encontrado!", Toast.LENGTH_LONG)
+                        .show()
+                }
+                return false
+            }
 
 
-        retrofit.filtrarBemAvaliado().enqueue(object : Callback<List<RestauranteReviewDto>>{
+            override fun onQueryTextChange(newText: String?): Boolean {
+
+                listAdapter.filter.filter(newText)
+                listaRes.visibility = View.VISIBLE
+                return false
+            }
+
+        })
+
+        binding.barraPesquisa.setOnCloseListener {
+            listaRes.visibility = View.INVISIBLE
+            false
+        }
+
+        retrofit.filtrarBemAvaliado().enqueue(object : Callback<List<RestauranteReviewDto>> {
             override fun onResponse(
                 call: Call<List<RestauranteReviewDto>>,
                 response: Response<List<RestauranteReviewDto>>
@@ -79,12 +143,12 @@ class MenuPrincipal : AppCompatActivity() {
             }
 
             override fun onFailure(call: Call<List<RestauranteReviewDto>>, t: Throwable) {
-                Log.d("T_RESPONSE_ERRO",t.toString())
+                Log.d("T_RESPONSE_ERRO", t.toString())
             }
 
         })
 
-        retrofit.filtrarEspecialidade().enqueue(object:Callback<List<String>>{
+        retrofit.filtrarEspecialidade().enqueue(object : Callback<List<String>> {
             override fun onResponse(
                 call: Call<List<String>>,
                 response: Response<List<String>>
@@ -95,7 +159,7 @@ class MenuPrincipal : AppCompatActivity() {
             }
 
             override fun onFailure(call: Call<List<String>>, t: Throwable) {
-                Log.d("T_RESPONSE_ERRO",t.toString())
+                Log.d("T_RESPONSE_ERRO", t.toString())
             }
         })
     }
--- app/src/main/java/com/example/mobile_application/service/RestauranteService.kt
+++ app/src/main/java/com/example/mobile_application/service/RestauranteService.kt
@@ -1,5 +1,6 @@
 package com.example.mobile_application.service
 
+import com.example.mobile_application.models.RestauranteDto
 import com.example.mobile_application.models.RestauranteReviewDto
 import retrofit2.Call
 import retrofit2.http.GET
@@ -12,4 +13,8 @@ interface RestauranteService {
     @GET("restaurantes/filtrar/nome-especialiade")
     fun filtrarEspecialidade():
             Call<List<String>>
+
+    @GET("restaurantes")
+    fun pegarRestaurante():
+            Call<List<RestauranteDto>>
 }
